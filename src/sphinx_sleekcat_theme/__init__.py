305f524b88bf69cb9afc1e4ea4b35be29f954153
miili <isken@gfz-potsdam.de>
2017-01-04 14:59:12 +0100

    Updated theme

--- src/sphinx_sleekcat_theme/__init__.py
+++ src/sphinx_sleekcat_theme/__init__.py
@@ -1,6 +1,18 @@
 import os
+from sphinx.domains import python as pdom
 
-__version__ = "2.4.0"
+__version__ = "0.1.0"
+
+
+class GAttributePyClassmember(pdom.PyClassmember):
+    def get_signature_prefix(self, sig):
+        return u'\u2666 '
+
+
+pdom.PythonDomain.object_types['gattribute'] = pdom.ObjType(
+    pdom.l_('gattribute'), 'gattr', 'obj')
+pdom.PythonDomain.directives['gattribute'] = GAttributePyClassmember
+pdom.PythonDomain.roles['gattr'] = pdom.PyXRefRole()
 
 
 def get_html_theme_path():
